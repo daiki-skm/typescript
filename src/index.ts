92ef7b36b9c576452482b3f31e19e60433cb5b06
daiki328 <dk08sel11@i.softbank.jp>
2022-07-19 11:51:52 +0900

    add

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,43 @@ let c = {
   banana: b,
 }
 let d = c.apple * 4
+
+type Cat = {
+  name: string
+  purrs: boolean
+}
+
+type Dog = {
+  name: string
+  barks: boolean
+  wags: boolean
+}
+
+type CatOrDog = Cat | Dog
+
+type CatAndDog = Cat & Dog
+
+const e: CatOrDog = {
+  name: 'cat',
+  purrs: true
+}
+
+const f: CatOrDog = {
+  name: 'dog',
+  barks: true,
+  wags: true
+}
+
+const g: CatOrDog = {
+  name: 'cat',
+  purrs: true,
+  barks: true,
+  wags: true
+}
+
+const h: CatAndDog = {
+  name: 'cat',
+  purrs: true,
+  barks: true,
+  wags: true
+}
