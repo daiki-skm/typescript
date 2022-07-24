16016831805bd253ad72b8b5151ef6b1f6e88bb9
daiki328 <dk08sel11@i.softbank.jp>
2022-07-24 21:02:52 +0900

    add

--- src/index.ts
+++ src/index.ts
@@ -48,3 +48,37 @@ const h: CatAndDog = {
   barks: true,
   wags: true
 }
+
+interface BuildableRequest {
+  data?: object
+  method: 'get' | 'post'
+  url: string
+}
+
+class RequestBuilder2 {
+  data?: object
+  method?: 'get' | 'post'
+  url?: string
+
+  setData(data: object): this & Pick<BuildableRequest, 'data'> {
+    return Object.assign(this, {data})
+  }
+
+  setMethod(method: 'get' | 'post'): this & Pick<BuildableRequest, 'method'> {
+    return Object.assign(this, {method})
+  }
+
+  setURL(url: string): this & Pick<BuildableRequest, 'url'> {
+    return Object.assign(this, {url})
+  }
+
+  build(this: BuildableRequest) {
+    return this
+  }
+}
+
+new RequestBuilder2()
+  .setData({})
+  .setMethod('post') // これを削除してみてください！
+  .setURL('bar') // これを削除してみてください！
+  .build()
